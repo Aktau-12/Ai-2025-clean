7840f0bec241697111eb5151e6a6d9c3b6c35ee6
Aktau-12 <axetime@mail.ru>
2025-04-22 18:55:22 +0500

    Fix imports: Pages → pages

--- coretalents-frontend/src/pages/CoreTalentsTest.jsx
+++ coretalents-frontend/src/pages/CoreTalentsTest.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
-import CoreTalentsResults from "../Pages/CoreTalentsResults";
+import CoreTalentsResults from "../pages/CoreTalentsResults";
 
 export default function CoreTalentsTest() {
   const [questions, setQuestions] = useState([]);
